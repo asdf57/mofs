3bb5cca1c0349980fd75d48c62826e590473656a
asdf57 <53836417+asdf57@users.noreply.github.com>
2023-09-03 01:06:35 -0400

    Fix FS structure. Introduce mkdir functionality

--- main.c
+++ main.c
@@ -18,9 +18,10 @@ enum {
 typedef struct FSEntry {
   int magic;
   char name[128];
+  struct FSEntry *parent;
   union {
     struct dir {
-      struct FSEntry *subentries[128];
+      struct FSEntry *children[128];
       int entry_count;
     } dir;
     
@@ -31,15 +32,19 @@ typedef struct FSEntry {
 const FSEntry root[] = {
   {DIR, ".", NULL},
   {DIR, "..", NULL},
-  {DIR, "channel",
-    {{FIEL, "1", NULL}},
+  {DIR, "channel", .dir = {
+        {
+          &(FSEntry){FIEL, "1", NULL},
+        },
+        1
+      },
   },
 };
 
 int
 add_subentry(FSEntry *dir, FSEntry *e) {
   if (dir->dir.entry_count < DIR_ENTRY_MAX)
-    dir->dir.subentries[dir->dir.entry_count] = e;
+    dir->dir.children[dir->dir.entry_count] = e;
 
   return 0;
 }
@@ -68,6 +73,7 @@ static int do_getattr(const char *path, struct stat *st) {
 // Reads contents of directory
 static int do_readdir(const char *path, void *buffer, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi) {
   int i;
+
 
   for (i = 0; i < sizeof(root)/sizeof(root[0]); i++) {
     printf("%s\n", root[i].name);
@@ -101,10 +107,14 @@ static int do_read(const char *path, char *buffer, size_t size, off_t offset, st
   return strlen(selectedText) - offset;
 }
 
+static int do_mkdir(const char *path, mode_t mode) {
+}
+
 static struct fuse_operations operations = {
   .getattr = do_getattr,
   .readdir = do_readdir,
   .read    = do_read,
+  .mkdir   = do_mkdir
 };
 
 int main(int argc, char **argv) {
