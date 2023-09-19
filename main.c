befc85cf5d2f73d8068cfa2e8832e67c6d7d3d9f
asdf57 <53836417+asdf57@users.noreply.github.com>
2023-09-18 22:14:31 -0400

    Use new filesystem design

--- main.c
+++ main.c
@@ -13,222 +13,79 @@
   mofs - A statically allocated FS
 */
 
-#define DIR_ENTRY_MAX 128
-#define TABLE_SIZE 128
-#define MAX_ENTRIES 1024
+
+#define NUM_HANDLERS 13
 
 enum {
-  DIR = 1,
-  FIEL = 2,
+  MOFS_DIR = 1,
+  MOFS_FILE = 2,
 };
 
-typedef struct Qid {
-  unsigned long bucket;
-  char path[128];
-} Qid;
+enum FSop {
+	GETATTR 		= 1 << 0,
+    	READLINK 	= 1 << 1,
+    	MKNOD 		= 1 << 2,
+    	MKDIR 		= 1 << 3,
+    	UNLINK		= 1 << 4,
+    	RMDIR		= 1 << 5,
+    	SYMLINK		= 1 << 6,
+    	RENAME		= 1 << 7,
+    	LINK			= 1 << 8,
+    	CHMOD		= 1 << 9,
+    	CHOWN		= 1 << 10,
+    	TRUNCATE 	= 1 << 11,
+    	OPEN 		= 1 << 12,
+};
 
+int
+bitflag_to_idx(int val) {
+	int i;
 
-/*
-  Struct holding file metadata
-  - Used to check if path is to file or dir
-  - 
-*/
-typedef struct FSEntry {
-  int magic;
-  Qid qid;
-  struct FSEntry *entries[128];
-  int num_entries;
-  struct FSEntry *next;
+	if (val % 2 != 0) {
+		fprintf(stderr, "val not a pow of 2!\n");
+		return -1;
+	}
+
+	for (i = 0; 1 < val; val /= 2)
+		i++;
+
+	return i;
+}
+
+typedef struct {
+  void *handlers[NUM_HANDLERS];
 } FSEntry;
 
 
-/*
-  /
-  ====
-  - file create handler - dynamically registers new parent directory
-*/
-typedef struct handlers {
-  void (*file_create_handler)(void);
-  void (*read_handler)(void);
-} handlers;
-
-FSEntry *entries[TABLE_SIZE];
-FSEntry pool[MAX_ENTRIES];
-int next_free_node = 0;
-
-
-unsigned long
-get_hash(char *str) {
-    unsigned long hash;
-    int c;
-
-    hash = 5381;
-
-    while ((c = *str++))
-        hash = ((hash << 5) + hash) + c;
-
-    return hash;
+int fs_getattr(const char *path, struct stat *st) {
+	
 }
 
-unsigned long
-get_bucket(char *key) {
-  return get_hash(key) % TABLE_SIZE;
+int fs_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi) {
+	
 }
 
-void
-get_parent_entry(const char *path, char *res) {
-  if (path == NULL || strlen(path) == 0)
-    return;
-
-  if (*path != '/')
-    return;
-
-  strcpy(res, path);
-
-  for (res = res + 1; *res != '/' && *res != '\0'; res++);
-  *res = '\0';
+int fs_read(const char *path, char *buffer, size_t size, off_t offset, struct fuse_file_info *fi) {
+	
 }
 
-void
-add_entry(FSEntry *entries[], FSEntry entry) {
-  unsigned int bucket;
-
-  if (next_free_node >= MAX_ENTRIES) {
-    fprintf(stderr, "ENOMEM: Out of memory!");
-    return;
-  }
-
-  bucket = get_bucket(entry.qid.path);
-
-  // First add entry to global static allocation
-  pool[next_free_node++] = entry;
-
-  if (entries[bucket] == NULL) {
-    entries[bucket] = &pool[next_free_node-1];
-  } else {
-    pool[next_free_node-1].next = entries[bucket];
-    entries[bucket] = &pool[next_free_node-1];
-  }
+int fs_mknod(const char *path, mode_t mode, dev_t rdev) {
+	
 }
 
-FSEntry*
-lookup_entry(char *key) {
-  FSEntry *entry;
-  unsigned long bucket;
-
-  bucket = get_bucket(key);
-  entry =  entries[bucket];
-
-  while (entry != NULL && strcmp(key, entry->qid.path) != 0) {
-    entry = entry->next;
-  }
-
-  return entry;
-}
-
-int
-set_file_attr(FSEntry *entry, struct stat *st) {
-  int res;
-
-  res = 0;
-
-  st->st_uid = getuid();
-  st->st_gid = getgid();
-  st->st_atime = time(NULL);
-  st->st_mtime = time(NULL);
-  
-  if (entry->magic == DIR) {
-    st->st_mode = S_IFDIR | 0755;
-    st->st_nlink = 2;
-  } else if (entry->magic == FIEL) {
-    st->st_mode = S_IFREG | 0755;
-    st->st_nlink = 1;
-  } else
-    res = -ENOENT;
-
-  return res;
-}
-
-/*
-  Get attribute file hook
-*/
-static int
-do_getattr(const char *path, struct stat *st) {
-  int res;
-  FSEntry *entry;
-
-  res = 0;
-
-  // Get parent entry for the appropriate handler
-  char result[strlen(path)+1];
-  get_parent_entry(path, result);
-  printf("Path '%s' has parent '%s'\n", path, result);
-
-  // speed up searching to find FSEntry
-  // finding FSEntry will give us type.
-  if ((entry = lookup_entry(path)) == NULL) {
-    // fprintf(stderr, "Couldn't find entry for path %s\n", path);
-    return -ENOENT;
-  }
-  
-  printf("entry's stored path: %s\n", entry->qid.path);
-  printf("path: %s\n", entry->qid.path);
-  printf("bucket: %lu\n", entry->qid.bucket);
-
-  return set_file_attr(entry, st);
-}
-
-static int
-do_readdir(const char *path, void *buffer, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi) {  
-  if (strcmp(path, "/") != 0)
-    return -ENOENT;
-
-  filler(buffer, ".", NULL, 0);
-  filler(buffer, "..", NULL, 0);
-  filler(buffer, "channel", NULL, 0);
-
-  return 0;
-}
-
-static int
-do_read(const char *path, char *buffer, size_t size, off_t offset, struct fuse_file_info *fi) {
-  FSEntry *entry, *parent;
-  char parent_name[256];
-
-  get_parent_entry(path, parent_name);
-  entry = lookup_entry(parent_name);
-  printf("parent path: %s\n", entry->qid.path);  
-}
-
-static int
-fs_mknod(const char *path, mode_t mode, dev_t dev) {
-  FSEntry *entry;
-
-  if ((entry = lookup_entry(path)) == NULL) {
-    printf("Creating entry: %s\n", path);
-    add_entry(entries, (FSEntry) {DIR, (Qid) {get_bucket(path), path}, NULL, 0});
-  } else {
-    return -ENOENT;
-  }
-
-  return 0;
+static int fs_utimens(const char *path, const struct timespec ts[2]) {
+	
 }
 
 static struct fuse_operations operations = {
-  .getattr = do_getattr,
-  .readdir = do_readdir,
-  .read    = do_read,
+  .getattr = fs_getattr,
+  .readdir = fs_readdir,
+  .read    = fs_read,
   .mknod   = fs_mknod,
+  .utimens = fs_utimens,
 };
-
-void
-fs_init(FSEntry *entries[]) {
-  add_entry(entries, (FSEntry) {DIR, (Qid) {get_bucket("/"), "/"}, NULL, 0});
-  add_entry(entries, (FSEntry) {DIR, (Qid) {get_bucket("/channel"), "/channel"}, NULL, 0});
-}
 
 int
 main(int argc, char **argv) {
-  fs_init(entries);
   return fuse_main(argc, argv, &operations, NULL);
 }
