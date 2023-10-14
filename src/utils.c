e1b8eb7fb6fcaec02a787ef6e6ddef77b56a20a2
asdf57 <53836417+asdf57@users.noreply.github.com>
2023-10-14 01:25:48 -0400

    Add additional utility methods

--- src/utils.c
+++ src/utils.c
@@ -1,3 +1,6 @@
+#include <string.h>
+#include <stdio.h>
+
 int
 bitflag_to_idx(int val) {
 	int i;
@@ -7,4 +10,41 @@ bitflag_to_idx(int val) {
 			return i;
 
 	return -1;
+}
+
+void
+tokenize_path(char **tokens, char *path) {
+	char dup_path[strlen(path)+1];
+	char *token;
+	int i;
+
+	i = 0;
+	strcpy(dup_path, path);
+
+	token = strtok(dup_path, "/");
+	while (token != NULL) {
+		tokens[i++] = token;
+		token = strtok(NULL, "/");
+	}
+}
+
+char *
+get_entry_name(char *path) {
+	char *marker;
+	char *entry_name;
+
+	marker = path;
+	entry_name = NULL;
+
+	if (strcmp(marker, "/") == 0)
+		return marker;
+
+	for (; *marker != '\0'; marker++) {
+		if (*marker == '/')
+			entry_name = marker+1;
+	}
+
+	printf("path is: %s\n", path);
+
+	return entry_name;
 }
--- src/utils.h
+++ src/utils.h
@@ -1,1 +1,3 @@
 int bitflag_to_idx(int);
+void tokenize_path(char **tokens, char *path);
+char * get_entry_name(char *);
