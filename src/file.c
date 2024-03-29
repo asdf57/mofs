fec1716f52f18ee45cf4833ef91cf86185df7421
asdf57 <53836417+asdf57@users.noreply.github.com>
2024-03-28 20:05:53 -0400

    Add stat struct attribute to FSE struct. Add function to generate stat struct

--- src/file.c
+++ src/file.c
@@ -45,3 +45,25 @@ regentries(FSE *entries[], int n) {
         fsehashtab[hash] = entries[i];
     }
 }
+
+struct stat
+genstat(FST type) {
+    struct stat st;
+
+    st.st_uid = getuid();
+    st.st_gid = getgid();
+    st.st_atime = time(NULL);
+    st.st_mtime = time(NULL);
+
+    if (type == QDIR) {
+        st.st_mode = S_IFDIR | 0755;
+        st.st_size = 1337;
+        st.st_nlink = 2;
+    } else {
+        st.st_mode = S_IFREG | 0644;
+        st.st_size = 4096;
+        st.st_nlink = 1;
+    }
+
+    return st;
+}
--- src/file.h
+++ src/file.h
@@ -3,6 +3,9 @@
 
 #include <stdlib.h>
 #include <stddef.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <unistd.h>
 #include "logger.h"
 #include "hash.h"
 
@@ -24,6 +27,7 @@ struct FSE {
     FSE *parent;
     FST type;
     struct fuse_operations *handlers;
+    struct stat properties;
 };
 
 extern FSE *fsehashtab[NENTRIES];
@@ -31,5 +35,6 @@ extern FSE *fsehashtab[NENTRIES];
 FSE *getfse(const char *);
 FSE *regentry(FSE);
 void regentries(FSE *[], int);
+struct stat genstat(FST type);
 
 #endif
